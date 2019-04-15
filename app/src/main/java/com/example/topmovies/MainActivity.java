f28caf0e46ef5aad06e8772e1f99e19f3ddb4290
dmk78@inbox.ru <Ek193burg>
2019-04-15 17:19:27 +0500

    выгружаем в JSON и помещаем в ARRayList<Movie>

--- app/src/main/java/com/example/topmovies/MainActivity.java
+++ app/src/main/java/com/example/topmovies/MainActivity.java
@@ -4,9 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.example.topmovies.data.Movie;
+import com.example.topmovies.utils.JSONUtils;
 import com.example.topmovies.utils.NetworkUtils;
 
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +18,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.POPULARITY, 5);
+        ArrayList<Movie> movies = JSONUtils.getMoviesFromJSON(jsonObject);
+        StringBuilder builder = new StringBuilder();
+        for (Movie movie : movies) {
+            builder.append(movie.getTitle()).append("\n");
+        }
+Log.i("MyResult",builder.toString());
 
     }
 }
