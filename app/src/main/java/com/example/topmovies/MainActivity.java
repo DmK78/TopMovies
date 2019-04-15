f82589c5f79cd24aa557f1d72d903e6129028d13
dmk78@inbox.ru <Ek193burg>
2019-04-15 21:28:01 +0500

    общие настройки

--- app/src/main/java/com/example/topmovies/MainActivity.java
+++ app/src/main/java/com/example/topmovies/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.topmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.topmovies.data.Movie;
 import com.example.topmovies.utils.JSONUtils;
@@ -13,18 +19,78 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerViewPosters;
+    private MovieAdapter movieAdapter;
+    private Switch switchSort;
+    private TextView textViewPopularitySort;
+    private TextView textViewTopRatedSort;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(NetworkUtils.POPULARITY, 5);
-        ArrayList<Movie> movies = JSONUtils.getMoviesFromJSON(jsonObject);
-        StringBuilder builder = new StringBuilder();
-        for (Movie movie : movies) {
-            builder.append(movie.getTitle()).append("\n");
+        switchSort = findViewById(R.id.switchSort);
+        recyclerViewPosters = findViewById(R.id.recyclerViewPosters);
+        textViewPopularitySort = findViewById(R.id.textViewPopularity);
+        textViewTopRatedSort = findViewById(R.id.textViewTopRated);
+
+
+        recyclerViewPosters.setLayoutManager(new GridLayoutManager(this, 2));
+        movieAdapter = new MovieAdapter();
+        switchSort.setChecked(true);
+        recyclerViewPosters.setAdapter(movieAdapter);
+        switchSort.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                setMethodOfSort(isChecked);
+
+
+            }
+        });
+
+        switchSort.setChecked(false);
+        movieAdapter.setOnPosterClickListener(new MovieAdapter.OnPosterClickListener() {
+            @Override
+            public void onPosterClick(int position) {
+                Toast.makeText(MainActivity.this, "Clicked"+position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        movieAdapter.setOnReachOfEndListener(new MovieAdapter.OnReachOfEndListener() {
+            @Override
+            public void onReachEnd() {
+                Toast.makeText(MainActivity.this, "Конец списка", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void onClickSetPopularity(View view) {
+        setMethodOfSort(false);
+        switchSort.setChecked(false);
+
+    }
+
+    public void onClickSetTopRated(View view) {
+        setMethodOfSort(true);
+        switchSort.setChecked(true);
+    }
+
+    public void setMethodOfSort(boolean isTopRated) {
+
+        int methodOfSort;
+        if (isTopRated) {
+            methodOfSort = NetworkUtils.TOP_RATED;
+            textViewTopRatedSort.setTextColor(getResources().getColor(android.R.color.holo_orange_dark));
+            textViewPopularitySort.setTextColor(getResources().getColor(android.R.color.white));
+        } else {
+            methodOfSort = NetworkUtils.POPULARITY;
+            textViewTopRatedSort.setTextColor(getResources().getColor(android.R.color.white));
+            textViewPopularitySort.setTextColor(getResources().getColor(android.R.color.holo_orange_dark));
         }
-Log.i("MyResult",builder.toString());
+
+        JSONObject jsonObject = NetworkUtils.getJSONFromNetwork(methodOfSort, 1);
+        ArrayList<Movie> movies = JSONUtils.getMoviesFromJSON(jsonObject);
+        movieAdapter.setMovies(movies);
 
     }
 }
--- app/src/main/java/com/example/topmovies/utils/JSONUtils.java
+++ app/src/main/java/com/example/topmovies/utils/JSONUtils.java
@@ -10,6 +10,10 @@ import java.util.ArrayList;
 
 public class JSONUtils {
 
+    public static final String BASE_POSTER_URL = "https://image.tmdb.org/t/p/";
+    public static final String SMALL_POSTER_SIZE = "w185";
+    public static final String BIG_POSTER_SIZE = "w780";
+
     private static final String KEY_RESULTS = "results";
     private static final String KEY_VOTE_COUNT = "vote_count";
     private static final String KEY_ID = "id";
@@ -23,23 +27,24 @@ public class JSONUtils {
 
     public static ArrayList<Movie> getMoviesFromJSON(JSONObject jsonObject) {
         ArrayList<Movie> result = new ArrayList<>();
-        if (jsonObject==null){
+        if (jsonObject == null) {
             return result;
         }
         try {
             JSONArray jsonArray = jsonObject.getJSONArray(KEY_RESULTS);
-            for (int i=0;i<jsonArray.length();i++){
+            for (int i = 0; i < jsonArray.length(); i++) {
                 JSONObject objectMovie = jsonArray.getJSONObject(i);
                 int id = objectMovie.getInt(KEY_ID);
                 int voteCount = objectMovie.getInt(KEY_VOTE_COUNT);
                 String title = objectMovie.getString(KEY_TITLE);
                 String originalTitle = objectMovie.getString(KEY_ORIGINAL_TITLE);
                 String overview = objectMovie.getString(KEY_OVERVIEW);
-                String posterPath = objectMovie.getString(KEY_POSTER_PATH);
+                String posterPath = BASE_POSTER_URL + SMALL_POSTER_SIZE + objectMovie.getString(KEY_POSTER_PATH);
+                String bigPosterPath = BASE_POSTER_URL + BIG_POSTER_SIZE + objectMovie.getString(KEY_POSTER_PATH);
                 String backdropPath = objectMovie.getString(KEY_BACKDROP_PATH);
                 double voteAverage = objectMovie.getDouble(KEY_VOTE_AVERAGE);
                 String releaseDate = objectMovie.getString(KEY_RELEASE_DATE);
-                Movie movie = new Movie(id,voteCount,title,originalTitle,overview,posterPath,backdropPath,voteAverage,releaseDate);
+                Movie movie = new Movie(id, voteCount, title, originalTitle, overview, posterPath, bigPosterPath, backdropPath, voteAverage, releaseDate);
                 result.add(movie);
 
 
