48b050caa340fb479a4999efbe5162b66307629f
dmk78@inbox.ru <Ek193burg>
2019-04-16 09:24:10 +0500

    изменили класс для работы с БД

--- app/src/main/java/com/example/topmovies/data/Movie.java
+++ app/src/main/java/com/example/topmovies/data/Movie.java
@@ -1,6 +1,11 @@
 package com.example.topmovies.data;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity (tableName = "movies")
 public class Movie {
+    @PrimaryKey
     private int id;
     private int voteCount;
     private String title;
